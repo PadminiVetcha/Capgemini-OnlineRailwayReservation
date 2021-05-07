69aecae90dcc5629cea6e51c44520af5acc87012
vetch <vetchapaddu13@gmail.com>
2021-05-07 17:08:47 +0530

    Ticket Management Service

---
+++ TicketMgmtService/src/main/java/org/padmini/railway/entity/PaymentDetails.java
@@ -0,0 +1,87 @@
+package org.padmini.railway.entity;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.springframework.data.annotation.Id;
+public class PaymentDetails 
+{
+	
+	@NotNull
+	@Size(min=16,max=16,message="Card Number should of 16 digits")
+	private String cardNo;
+	
+	@Id
+	@NotNull
+	private long pnrNo;
+	
+	@NotNull
+	@Max(value=999,message="CVV cannot exceed 3 digits")
+	private int cvv;
+	
+	@NotNull
+	private String bankName;
+	
+	@NotNull
+	@Min(value=10,message="Please enter amount correctly..!!")
+	private int amt;
+	
+	public PaymentDetails() {
+		super();
+	}
+
+	public PaymentDetails(String cardNo, long pnrNo,int cvv, String bankName, int amt) {
+		super();
+		this.cardNo = cardNo;
+		this.pnrNo=pnrNo;
+		this.cvv = cvv;
+		this.bankName = bankName;
+		this.amt = amt;
+	}
+
+	public String getCardNo() {
+		return cardNo;
+	}
+
+	public void setCardNo(String cardNo) {
+		this.cardNo = cardNo;
+	}
+	
+	public long getPnrNo() {
+		return pnrNo;
+	}
+
+	public void setPnrNo(long pnrNo) {
+		this.pnrNo = pnrNo;
+	}
+
+	public int getCvv() {
+		return cvv;
+	}
+
+	public void setCvv(int cvv) {
+		this.cvv = cvv;
+	}
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+
+	public int getAmt() {
+		return amt;
+	}
+
+	public void setAmt(int amt) {
+		this.amt = amt;
+	}
+
+	@Override
+	public String toString() {
+		return "PaymentDetails [cardNo=" + cardNo + ", pnrNo=" + pnrNo + ", cvv=" + cvv + ", bankName=" + bankName
+				+ ", amt=" + amt + "]";
+	}
+}
