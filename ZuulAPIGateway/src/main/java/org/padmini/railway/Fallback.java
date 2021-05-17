f0eb0fa78e808429628467f16f2512b5ebd71a2d
vetch <vetchapaddu13@gmail.com>
2021-05-17 08:57:50 +0530

    Made Changes

--- ZuulAPIGateway/src/main/java/org/padmini/railway/Fallback.java
+++ ZuulAPIGateway/src/main/java/org/padmini/railway/Fallback.java
@@ -1,15 +1,13 @@
-package org.padmini.railway;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class Fallback  
-{
-	@RequestMapping("/userFallback")
-	public String userFallback()
-	{
-		return "Please wait.!! It may take some time..!!!";
-	}
-   
-
-}
+/*
+ * package org.padmini.railway; import
+ * org.springframework.web.bind.annotation.RequestMapping; import
+ * org.springframework.web.bind.annotation.RestController;
+ * 
+ * @RestController public class Fallback {
+ * 
+ * @RequestMapping("/userFallback") public String userFallback() { return
+ * "Please wait.!! It may take some time..!!!"; }
+ * 
+ * 
+ * }
+ */
